2bcf159ea636a1996cf1eb19943baddd5ccf1021
Conslatekoyo <conslatekoyo@gmail.com>
2022-05-16 09:21:36 +0300

    original commit

---
+++ app/src/main/java/com/example/myapplication/CalculatorActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class CalculatorActivity : AppCompatActivity() {
+    lateinit var btnAdd:Button
+    lateinit var btnSubstract:Button
+    lateinit var btnMultiply:Button
+    lateinit var btnModulus:Button
+    lateinit var tvTotal:TextView
+    lateinit var etFirstNumber:EditText
+    lateinit var etSecondNumber:EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+        btnAdd=findViewById(R.id.btnAdd)
+        btnSubstract=findViewById(R.id.btnSubstract)
+        btnModulus=findViewById(R.id.btnModulus)
+        btnMultiply=findViewById(R.id.btnMultiply)
+        tvTotal=findViewById(R.id.tvTotal)
+
+        btnAdd.setOnClickListener {
+            tvTotal.text=""
+            val number1=etFirstNumber.text.toString().toDouble()
+            val number2=etSecondNumber.text.toString().toDouble()
+            addition(number1,number2)
+        }
+        btnSubstract.setOnClickListener {
+            tvTotal.text=""
+            val number1=etFirstNumber.text.toString().toDouble()
+            val number2=etSecondNumber.text.toString().toDouble()
+            substraction(number1,number2)
+        }
+        btnMultiply.setOnClickListener {
+            tvTotal.text=""
+            val number1=etFirstNumber.text.toString().toDouble()
+            val number2=etSecondNumber.text.toString().toDouble()
+            multiplication(number1,number2)
+        }
+        btnModulus.setOnClickListener {
+            tvTotal.text=""
+            val number1=etFirstNumber.text.toString().toDouble()
+            val number2=etSecondNumber.text.toString().toDouble()
+            modulus(number1,number2)
+        }
+    }
+    fun addition(number1:Double,number2:Double){
+        val add=number1+number2
+        tvTotal.text=add.toString()
+
+    }
+    fun substraction(number1:Double,number2:Double){
+        val subtract=number1-number2
+        tvTotal.text=subtract.toString()
+
+    }
+    fun multiplication(number1:Double,number2:Double){
+        val multiply=number1*number2
+        tvTotal.text=multiply.toString()
+
+    }
+    fun modulus(number1:Double,number2:Double){
+        val remainder=number1%number2
+        tvTotal.text=remainder.toString()
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnCalculate:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnCalculate=findViewById(R.id.btnCalculate)
+        btnCalculate.setOnClickListener {
+            val intent=Intent(this,CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
